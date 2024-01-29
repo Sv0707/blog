f52b3fb8eab36efcb5be2ebbdc8b69bd618b112e
Svitlana <87758266+Sv0707@users.noreply.github.com>
2024-01-29 22:08:30 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Posts from './pages/posts/Posts'
 import Albums from './pages/albums/Albums'
@@ -7,13 +7,13 @@ import '../src/ui-kit/styles/core.scss'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<Home />} path="blog/" exact />
         <Route element={<Posts />} path="blog/posts/:id" />
         <Route element={<Albums />} path="blog/albums/:id" />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
