9c7ddeb36cc5dd317a8d767759157b2496c5b095
Svitlana <87758266+Sv0707@users.noreply.github.com>
2024-01-30 13:45:16 +0200

    added react-snap

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { hydrate, render } from 'react-dom'
 
-
-const container = document.getElementById('root')
-if (container.hasChildNodes()) {
-  hydrate(<App />, container)
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
 } else {
-  render(<App />, container)
+  render(<App />, rootElement)
 }
 
 // If you want to start measuring performance in your app, pass a function
